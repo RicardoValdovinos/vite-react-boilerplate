4ff5810bb95c397442f3b82c25bd310950be8aff
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-01-31 23:45:35 -0700

    build(vite.config.ts): added TanStackRouterVite plugin for file-based routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), TanStackRouterVite()],
 	server: {
 		host: true,
 		strictPort: true,
