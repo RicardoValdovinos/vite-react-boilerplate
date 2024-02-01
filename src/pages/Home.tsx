fd5ea2c9e860c4293c6af8761bb04db383dee378
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-01-31 23:52:26 -0700

    refactor(home.tsx): changed default export to named export

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import type { FunctionComponent } from "../common/types";
 
-const Home = (): FunctionComponent => {
+export const Home = (): FunctionComponent => {
 	return (
 		<div className="bg-blue-300  font-bold w-screen h-screen flex flex-col justify-center items-center ">
 			<p className="text-white text-6xl">Hello, world!</p>
 		</div>
 	);
 };
-
-export default Home;
