e3e098aa56de8a07f8bf2c9fb8637741cdba58f6
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-07-31 13:51:44 -0700

    refactor(prettier.config.js): converted prettier config from .json to .js

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+const prettierConfig = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	quoteProps: "as-needed",
+	jsxSingleQuote: false,
+	trailingComma: "es5",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: "preserve",
+	htmlWhitespaceSensitivity: "css",
+	endOfLine: "lf",
+	embeddedLanguageFormatting: "auto",
+	singleAttributePerLine: false
+}
+
+export default prettierConfig
