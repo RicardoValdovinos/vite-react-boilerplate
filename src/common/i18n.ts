f9aab1672e19e056019ba603957ff10117c73a24
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-08-08 11:52:22 -0700

    style: formatted code

--- src/common/i18n.ts
+++ src/common/i18n.ts
@@ -4,16 +4,16 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import Backend, { type HttpBackendOptions } from "i18next-http-backend";
 
 const i18nOptions: InitOptions<HttpBackendOptions> = {
-    ns: ["home"],
-    fallbackLng: "en",
-    debug: true,
-    interpolation: {
-        escapeValue: false, // not needed for react as it escapes by default
-    }
-}
+	ns: ["home"],
+	fallbackLng: "en",
+	debug: true,
+	interpolation: {
+		escapeValue: false, // not needed for react as it escapes by default
+	},
+};
 
 await i18n
-    .use(initReactI18next)
-    .use(LanguageDetector)
-    .use(Backend)
-    .init<HttpBackendOptions>(i18nOptions);
+	.use(initReactI18next)
+	.use(LanguageDetector)
+	.use(Backend)
+	.init<HttpBackendOptions>(i18nOptions);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,17 +5,16 @@ export const Home = (): FunctionComponent => {
 	const { t, i18n } = useTranslation();
 
 	const onTranslateButtonClick = async (): Promise<void> => {
-		if (i18n.resolvedLanguage === "en")
+		if (i18n.resolvedLanguage === "en") {
 			await i18n.changeLanguage("es");
-		else
+		} else {
 			await i18n.changeLanguage("en");
+		}
 	};
 
 	return (
 		<div className="bg-blue-300  font-bold w-screen h-screen flex flex-col justify-center items-center">
-			<p className="text-white text-6xl">
-				{t("greeting", {ns: 'home'})}
-			</p>
+			<p className="text-white text-6xl">{t("greeting", { ns: "home" })}</p>
 			<button type="submit" onClick={onTranslateButtonClick}>
 				translate
 			</button>
