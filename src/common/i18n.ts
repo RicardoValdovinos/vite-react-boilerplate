c453a14c4f670cf482d3846fcf6f3675dd3f0dfe
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-08-08 20:57:31 -0700

    style: formatted code

--- src/common/i18n.ts
+++ src/common/i18n.ts
@@ -2,13 +2,13 @@ import i18n, { type InitOptions } from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend, { type HttpBackendOptions } from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
-import translationEN from '../assets/locales/en/translations.json';
-import translationES from '../assets/locales/es/translations.json';
+import translationEN from "../assets/locales/en/translations.json";
+import translationES from "../assets/locales/es/translations.json";
 
 export const defaultNS = "translations";
 export const resources = {
 	en: { translations: translationEN },
-	es: { translations: translationES }
+	es: { translations: translationES },
 } as const;
 
 const i18nOptions: InitOptions<HttpBackendOptions> = {
@@ -21,7 +21,7 @@ const i18nOptions: InitOptions<HttpBackendOptions> = {
 	},
 	backend: {
 		loadPath: "src/assets/locales/{{lng}}/translations.json",
-	}
+	},
 };
 
 void i18n
