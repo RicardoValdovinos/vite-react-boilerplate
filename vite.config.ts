5217e7b00ef0352b2db370543616ba7fc668feae
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-08-09 11:24:44 -0700

    build(vite.config.ts): updated tanstack router vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 import { normalizePath } from "vite";
