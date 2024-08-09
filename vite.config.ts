fbaea540551da1f48ce82ecf6c889cde5d0af617
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-08-08 21:52:39 -0700

    build(vite.config.ts): using viteStaticCopy to copy locale files to dist folder on project build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
+	plugins: [react(), TanStackRouterVite(), 
+		viteStaticCopy({
+		targets: [
+		  {
+			src: 'src/assets/locales',
+			dest: '.'
+		  }
+		]
+	  })],
 	server: {
 		host: true,
 		strictPort: true,
